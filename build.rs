35a85bb125ae6760daf7b8159c17682df33e7fad
mulmuri <gyumin.oh@payhere.in>
2024-06-09 00:09:51 +0900

    build.rs 오류 수정

--- build.rs
+++ build.rs
@@ -2,11 +2,14 @@ use std::env;
 use dotenv::dotenv;
 
 fn main() {
-    dotenv().expect("Failed to read .env file");
+    #[cfg(not(debug_assertions))]
+    {
+        dotenv().expect("Failed to read .env file");
 
-    env::var("INFLUXDB_URL").expect("INFLUX_URL must be set");
-    env::var("INFLUXDB_BUCKET").expect("INFLUX_BUCKET must be set");
-    env::var("INFLUXDB_TOKEN").expect("INFLUX_TOKEN must be set");
-    env::var("INFLUXDB_ORG").expect("INFLUX_ORG must be set");
-    env::var("TEXT_FILE_PATH").expect("TEXT_FILE_PATH must be set");
+        env::var("INFLUXDB_URL").expect("INFLUX_URL must be set");
+        env::var("INFLUXDB_BUCKET").expect("INFLUX_BUCKET must be set");
+        env::var("INFLUXDB_TOKEN").expect("INFLUX_TOKEN must be set");
+        env::var("INFLUXDB_ORG").expect("INFLUX_ORG must be set");
+        env::var("TEXT_FILE_PATH").expect("TEXT_FILE_PATH must be set");
+    }
 }
